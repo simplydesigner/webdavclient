ab405e96f4e0683a97aeee8011b091c9619b55aa
designerror <designerror@yandex.ru>
2014-10-28 19:48:52 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+from setuptools.command.test import test as TestCommand
 from setuptools import setup, find_packages
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+    def initialize_options(self):
+        TestCommand.initialize_options(self) 
+        self.pytest_args = []
+
+    def finalize_options(self): 
+        TestCommand.finalize_options(self) 
+        self.test_args = [] 
+        self.test_suite = True
+
+def run_tests(self):
+#import here, cause outside the eggs aren't loaded import pytest
+    errno = pytest.main(self.pytest_args) 
+    sys.exit(errno)
 
 setup(
     name     = 'webdavclient',
@@ -25,4 +43,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    tests_require=['pytest'],
+    cmdclass = {'test': PyTest},
 )
